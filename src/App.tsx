4fdc9a47c9571ab4b9df8603e9f0331ef6d7c981
SeungGukYoo <dbtmd878@gmail.com>
2023-06-05 14:15:22 +0900

    콘텐츠 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Layout from "./Layout";
 import Header from "./components/header/Header";
-import Main from "./components/main/Main";
+import Description1 from "./components/main/Description1";
+import Description2 from "./components/main/Description2";
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Header />
-        <Main />
+        <Description1 />
+        <Description2 />
       </Layout>
     </div>
   );
