4d4969aa9f99cfa9f44b9779861f965d2f41f133
SeungGukYoo <dbtmd878@gmail.com>
2023-06-05 15:14:57 +0900

    스타일 코드 flex로 변경

--- src/components/main/Description1.tsx
+++ src/components/main/Description1.tsx
@@ -3,15 +3,15 @@ import React from "react";
 const Description1 = () => {
   return (
     <main className="pt-[50vh]">
-      <section className="h-[4000px] relative w-full border-red-400 border-4 text-center">
-        <h1 className="text-[4rem] lg:text-[9vw] font-medium">IPhone 13 Pro</h1>
-        <div className="absolute h-[3em] bg-slate-400 -top-[10vh] left-0 w-full text-center leading-snug opacity-1">
+      <section className="h-[4000px] relative w-full text-center">
+        <h1 className="relative text-[4rem] -top-[10vh]  lg:text-[9vw] font-medium">IPhone 13 Pro</h1>
+        <div className="fixed h-[3em] flex justify-center items-center left-0 leading-snug top-[35vh] opacity-0">
           <p className="text-[4rem]">프로 그 이상.</p>
         </div>
-        <div className="absolute h-[3em] w-full -top-[10vh] left-0 text-center leading-snug	opacity-0">
+        <div className="fixed h-[3em] w-full left-0 flex justify-center items-center top-[35vh] leading-snug	opacity-0">
           <p className="text-[4rem]">그 어떤 스마트폰 글래스보다 더 견고한 소재.</p>
         </div>
-        <div className="absolute h-[3em] w-full -top-[10vh] left-0 text-center leading-snug	opacity-0">
+        <div className="fixed h-[3em] top-[35vh] left-0 leading-snug flex justify-center items-center opacity-0">
           <p className="text-[4rem]">격이 다른 카메라의 격이 다른 셀피.</p>
         </div>
       </section>
