7e07fa68ebee7be03e5a8034a5e39bb3309ac181
SeungGukYoo <dbtmd878@gmail.com>
2023-06-05 15:36:46 +0900

    footer추가

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="flex items-center justify-center h-[7rem] bg-slate-700 text-white">2023. SeungGuk</div>;
+};
+
+export default Footer;
