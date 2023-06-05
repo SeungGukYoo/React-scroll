88d8d23c5f50c3cb7b42195c2477434f787127d4
SeungGukYoo <dbtmd878@gmail.com>
2023-06-05 15:37:56 +0900

    4번째 컴포넌트 생성

---
+++ src/components/main/Description4.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Description4 = () => {
+  return (
+    <section className="m-auto h-[4000px] flex flex-col items-center">
+      <p className="max-w-[1024px] mx-auto text-[2rem] text-gray-400">
+        <strong className="text-black">A16 Bionic 칩</strong>
+        <br />
+        사진속 모든 것이 가장 아름답게
+        <br />
+        보이도록 해줍니다.
+      </p>
+      <p className="max-w-[1024px] text-[1.2rem] text-gray-400 px-1 mx-auto ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, quis architecto. Doloribus sit officia maiores
+        quo ad magni dignissimos inventore, placeat hic. Eos ducimus assumenda id sed, tempore provident nemo accusamus
+        similique eius quisquam maiores totam aspernatur blanditiis fugit laudantium delectus nisi. Perferendis eum
+        totam pariatur qui voluptates sunt iste odio modi magnam voluptas, quidem explicabo? Tempore aperiam nobis sunt
+        minus laudantium nisi est quos nostrum, saepe magni minima, aliquam esse, fugiat facere. Deleniti magnam
+        repellendus perspiciatis cum ex similique accusantium! Sequi quia animi, culpa corporis magni rerum consectetur
+        tempora aperiam repellat pariatur commodi facere, consequatur quae minima magnam fugit.
+      </p>
+    </section>
+  );
+};
+
+export default Description4;
