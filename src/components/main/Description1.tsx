bbc668f0ab0f335e2066fa5c3739588e9cd4ffe0
SeungGukYoo <dbtmd878@gmail.com>
2023-06-07 17:21:26 +0900

    스크롤에 따라 문구의 opacity 조절 로직 추가

--- src/components/main/Description1.tsx
+++ src/components/main/Description1.tsx
@@ -1,22 +1,40 @@
-import React, { SetStateAction, useContext, useEffect, useState } from "react";
+import React, { SetStateAction, useEffect, useState } from "react";
 
 import SceneContainer from "./SceneContainer";
 
 interface DescriptionProps {
-  clientHeight: number[];
+  clientScrollY: number;
   setClientHeight: React.Dispatch<SetStateAction<number[]>>;
+  ratio: number;
 }
+type InfoType = [number, number, { start: number; end: number }];
 
-const Description1 = ({ clientHeight, setClientHeight }: DescriptionProps) => {
-  const [curentHeight, setCurEentHeigth] = useState(0);
+const eventInfo: InfoType = [0, 1, { start: 0.3, end: 0.4 }];
+
+const Description1 = ({ clientScrollY, setClientHeight, ratio }: DescriptionProps) => {
+  const [currentHeight, setCurEentHeigth] = useState(0);
+
+  // start 픽셀 높이값,end 픽셀 높이값
+  const eventStart = eventInfo[2].start * currentHeight;
+  const eventEnd = eventInfo[2].end * currentHeight;
+  const eventHeight = eventEnd - eventStart;
+  // console.log(clientScrollY / currentHeight);
+  // console.log(eventStart, eventEnd, clientScrollY);
+
+  let rv = 0;
+
+  if (clientScrollY >= eventStart && clientScrollY <= eventEnd) {
+    rv = ((clientScrollY - eventStart) / eventHeight) * (eventInfo[1] - eventInfo[0]) + eventInfo[0];
+  } else if (clientScrollY < eventStart) rv = eventInfo[0];
+  else if (clientScrollY > eventEnd) rv = eventInfo[1];
 
   useEffect(() => {
     setClientHeight((pre: number[]) => {
       const newArr = [...pre];
-      newArr[0] = curentHeight;
+      newArr[0] = currentHeight;
       return newArr;
     });
-  }, [curentHeight]);
+  }, [currentHeight]);
   return (
     <SceneContainer heightNum={5} currentHeight={(height) => setCurEentHeigth(height)}>
       <h1 className="relative text-[4rem] -top-[10vh]  lg:text-[9vw] font-medium flex items-center justify-center">
@@ -25,7 +43,7 @@ const Description1 = ({ clientHeight, setClientHeight }: DescriptionProps) => {
       <div className="sticky-elem main-message left-0 leading-snug w-full">
         <p className="w-fulltext-center">프로 그 이상.</p>
       </div>
-      <div className="sticky-elem main-message w-full left-0  leading-snug">
+      <div className="sticky-elem main-message w-full left-0 leading-snug" style={{ opacity: `${rv}` }}>
         <p className="w-full text-center">
           그 어떤 스마트폰 글래스보다
           <br /> 더 견고한 소재.
