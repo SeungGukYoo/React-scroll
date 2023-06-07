3669ff1307a679bdfe2a19b32489518357b74153
SeungGukYoo <dbtmd878@gmail.com>
2023-06-07 17:20:19 +0900

    스크롤 비율, 컴포넌트 내의 스크롤 값 전달

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Description1 from "./components/main/Description1";
@@ -11,14 +10,16 @@ function App() {
   const [clientHeight, setClientHeight] = useState<number[]>([0, 0, 0, 0]);
   const [sceneInfo, setSceneInfo] = useState(0);
   const [preHeight, setPreHeight] = useState(0);
-
+  const [ratio, setRatio] = useState(0);
   let scrollY = useRef(0);
+  let client_scrollY = useRef(0);
   let isScrolling = useRef(false);
 
   useEffect(() => {
+    // scroll event 최적화 안됨
     const scrollEvent = () => {
       scrollY.current = window.scrollY;
-      console.log(scrollY.current, preHeight, sceneInfo);
+
       if (scrollY.current > preHeight + clientHeight[sceneInfo] && !isScrolling.current) {
         isScrolling.current = true;
         setPreHeight((pre) => (pre += clientHeight[sceneInfo]));
@@ -35,6 +36,9 @@ function App() {
           isScrolling.current = false;
         }, 200);
       }
+      client_scrollY.current = scrollY.current - preHeight;
+
+      setRatio((pre) => (scrollY.current - preHeight) / clientHeight[sceneInfo]);
     };
 
     window.addEventListener("scroll", scrollEvent);
@@ -47,7 +51,7 @@ function App() {
   return (
     <div>
       <Header />
-      <Description1 clientHeight={clientHeight} setClientHeight={setClientHeight} />
+      <Description1 ratio={ratio} clientScrollY={client_scrollY.current} setClientHeight={setClientHeight} />
       <Description2 clientHeight={clientHeight} setClientHeight={setClientHeight} />
       <Description3 clientHeight={clientHeight} setClientHeight={setClientHeight} />
       <Description4 clientHeight={clientHeight} setClientHeight={setClientHeight} />
