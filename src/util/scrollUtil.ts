92b6f91092fa9de1c176d96667709a6468a67844
SeungGukYoo <dbtmd878@gmail.com>
2023-06-13 00:35:57 +0900

    canvas일 경우 로직과 일반적인 로직 분리

--- src/util/scrollUtil.ts
+++ src/util/scrollUtil.ts
@@ -1,18 +1,23 @@
 import { ScrollEventInfo } from "./../../types/component.d";
+type SectionInfo = ScrollEventInfo | number[];
 
-const ScrollEvent = (section_info: ScrollEventInfo, currentHeight: number, clientScrollY: number) => {
-  // 현재 넓이의 이벤트 시작 값
-  const eventStart = section_info[2].start * currentHeight;
-  // 현재 넓이의 이벤트 끝 값
-  const eventEnd = section_info[2].end * currentHeight;
-  // 현재 넓이
-  const eventHeight = eventEnd - eventStart;
-
+const ScrollEvent = (section_info: SectionInfo | number[], currentHeight: number, clientScrollY: number) => {
+  const eventStart =
+    section_info[2] && typeof section_info[2] === "object" ? section_info[2]?.start * currentHeight : undefined;
+  const eventEnd =
+    section_info[2] && typeof section_info[2] === "object" ? section_info[2]?.end * currentHeight : undefined;
   let rv = 0;
-  if (clientScrollY >= eventStart && clientScrollY <= eventEnd) {
-    rv = ((clientScrollY - eventStart) / eventHeight) * (section_info[1] - section_info[0]) + section_info[0];
-  } else if (clientScrollY < eventStart) rv = section_info[0];
-  else if (clientScrollY > eventEnd) rv = section_info[1];
+  if (eventStart !== undefined && eventEnd !== undefined) {
+    const eventHeight = eventEnd - eventStart;
+
+    if (clientScrollY >= eventStart && clientScrollY <= eventEnd) {
+      rv = ((clientScrollY - eventStart) / eventHeight) * (section_info[1] - section_info[0]) + section_info[0];
+    } else if (clientScrollY < eventStart) rv = section_info[0];
+    else if (clientScrollY > eventEnd) rv = section_info[1];
+  } else {
+    const ratio = currentHeight / clientScrollY;
+    rv = ratio * (section_info[1] - section_info[0]) + section_info[0];
+  }
 
   return rv;
 };
