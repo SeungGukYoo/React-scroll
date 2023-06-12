8f2f419c3823195b038d62c9edee432c99d36949
SeungGukYoo <dbtmd878@gmail.com>
2023-06-12 14:27:59 +0900

    스크롤 Context 추가로 dispatch 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,96 +5,49 @@ import Description1 from "./components/main/Description1";
 import Description2 from "./components/main/Description2";
 import Description3 from "./components/main/Description3";
 import Description4 from "./components/main/Description4";
+import { useSharedValuesContext } from "./context";
 
 function App() {
   const [clientHeight, setClientHeight] = useState<number[]>([0, 0, 0, 0]);
-  const [sceneInfo, setSceneInfo] = useState(0);
-  const [preHeight, setPreHeight] = useState(0);
-  const [ratio, setRatio] = useState(0);
+  const { state, dispatch } = useSharedValuesContext();
   let scrollY = useRef(0);
-  let client_scrollY = useRef(0);
-  let isScrolling = useRef(false);
 
   useEffect(() => {
-    client_scrollY.current = scrollY.current - preHeight;
-  }, [preHeight]);
+    if (state.scrollValue > clientHeight[state.sceneInfo] + state.prevHeight) {
+      dispatch({
+        type: "INCREASE_PREV_HEIGHT",
+        payload: clientHeight[state.sceneInfo],
+      });
+      dispatch({
+        type: "INCREASE_SCENE_INFO",
+      });
+    } else if (state.scrollValue < state.prevHeight) {
+      dispatch({
+        type: "DECREASE_PREV_HEIGHT",
+        payload: clientHeight[state.sceneInfo - 1],
+      });
+      dispatch({
+        type: "DECREASE_SCENE_INFO",
+      });
+    }
+  }, [clientHeight, dispatch, state.prevHeight, state.sceneInfo, state.scrollValue]);
 
   useEffect(() => {
-    // scroll event 최적화 안됨
-
     const scrollEvent = () => {
       scrollY.current = window.scrollY;
-
-      if (scrollY.current > preHeight + clientHeight[sceneInfo] && !isScrolling.current) {
-        isScrolling.current = true;
-        setPreHeight((pre) => {
-          return (pre += clientHeight[sceneInfo]);
-        });
-        setSceneInfo((pre) => (pre += 1));
-
-        setTimeout(() => {
-          isScrolling.current = false;
-        }, 200);
-        return;
-      }
-      if (scrollY.current < preHeight && !isScrolling.current) {
-        isScrolling.current = true;
-        setPreHeight((pre) => {
-          return (pre -= clientHeight[sceneInfo - 1]);
-        });
-        setSceneInfo((pre) => (pre -= 1));
-        setTimeout(() => {
-          isScrolling.current = false;
-        }, 200);
-        return;
-      }
-
-      setRatio((pre) => {
-        let result = (scrollY.current - preHeight) / clientHeight[sceneInfo];
-        if (result < 0) result = 0;
-        else if (result > 1) result = 1;
-        pre = result;
-        return pre;
-      });
-
-      client_scrollY.current = scrollY.current - preHeight;
+      dispatch({ type: "SET_SCROLL_VALUE", payload: scrollY.current });
     };
-
     window.addEventListener("scroll", scrollEvent);
-
-    return () => {
-      window.removeEventListener("scroll", scrollEvent);
-    };
-  }, [clientHeight, sceneInfo, preHeight]);
+    return () => window.removeEventListener("scroll", scrollEvent);
+  }, [dispatch]);
 
   return (
     <div>
       <Header />
-      <Description1
-        sceneInfo={sceneInfo}
-        ratio={ratio}
-        clientScrollY={client_scrollY.current}
-        setClientHeight={setClientHeight}
-      />
-      <Description2
-        sceneInfo={sceneInfo}
-        ratio={ratio}
-        clientScrollY={client_scrollY.current}
-        setClientHeight={setClientHeight}
-      />
-      <Description3
-        sceneInfo={sceneInfo}
-        ratio={ratio}
-        clientScrollY={client_scrollY.current}
-        setClientHeight={setClientHeight}
-      />
-
-      <Description4
-        sceneInfo={sceneInfo}
-        ratio={ratio}
-        clientScrollY={client_scrollY.current}
-        setClientHeight={setClientHeight}
-      />
+      <Description1 setClientHeight={setClientHeight} />
+      <Description2 setClientHeight={setClientHeight} />
+      <Description3 setClientHeight={setClientHeight} />
+      <Description4 setClientHeight={setClientHeight} />y
       <Footer />
     </div>
   );
